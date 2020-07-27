5f648424b4a086523c71883390a10f8bca36433c
Sanchez <fsanchez@3inlniarora.ad.here.com>
2020-07-27 17:04:48 -0400

    Spring cleanup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,32 +33,8 @@ module.exports = {
     docsDir: '',
     editLinkText: '',
     lastUpdated: false,
-    nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
-      }
-    ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
-      ],
-    }
+    nav: [],
+    sidebar: {}
   },
 
   /**
@@ -68,4 +44,4 @@ module.exports = {
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
   ]
-}
+};
